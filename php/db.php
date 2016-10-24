fc30115e3e500b2a1daad78c194aff07ec2ea1cf
Aidan Breen <aido179@yahoo.com>
2016-10-25 00:22:29 +0100

    debug php

--- php/db.php
+++ php/db.php
@@ -8,14 +8,14 @@
   $clean_email = filter_var($original_email,FILTER_SANITIZE_EMAIL);
   if ($original_email == $clean_email && filter_var($original_email,FILTER_VALIDATE_EMAIL)){
     // now we know the original email was safe to insert.
-    $sql = "SELECT * FROM theGame WHERE email = {$clean_email}";
+    $sql = "SELECT * FROM theGame WHERE email = '{$clean_email}'";
     $data = mysql_query($sql) or die("Error - query 1 failed: {$sql}");
     $rowCount = mysql_num_rows($data);
     if($rowCount>0){
       $info = mysql_fetch_array($data);
       $clean_timestamp = filter_var($_POST['timestamp'],FILTER_SANITIZE_NUMBER_INT);
       $sql = "UPDATE theGame SET timestamp = '{$clean_timestamp}' WHERE email = {$clean_email}";
-      mysql_query($sql) or die("Error - query 2 failed.");
+      mysql_query($sql) or die("Error - query 2 failed: {$sql}");
       echo $info['timestamp'];
     }else{
       echo "Not found";
