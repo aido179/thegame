425889b29468559373aea7b261b84fbc8b3119fe
Aidan Breen <aido179@yahoo.com>
2016-10-25 00:04:01 +0100

    test1

--- script.js
+++ script.js
@@ -5,9 +5,24 @@ $(document).ready(function(){
 $('button').on('click',function(){
   $(".screen").hide();
   $("#loading_screen").show();
+  /*
   window.setTimeout(function(){
+
+  },1000);*/
+
+
+  $.ajax({
+    type: "POST",
+    url: "php/db.php",
+    data: {
+      email: $('#email-input').val(),
+      timestamp: Date.now()
+    }
+  })
+  .done(function( resp ) {
     $(".screen").hide();
     $("#results_screen").show();
-  },1000);
+    console.log(resp);
+  });
 
 });
